22433c199502e6b245df9120c6b4f53241d58b84
gaetano78 <g.la.delfa@alice.it>
2024-11-15 11:55:14 +0100

    Update CVRPGenUchoa.py

--- CVRPGenUchoa.py
+++ CVRPGenUchoa.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1nSozjnFgHEXC5bEHxJRrozDEAWylCCbs
 """
 
-!pip install streamlit
 import streamlit as st
 import sys
 import random
