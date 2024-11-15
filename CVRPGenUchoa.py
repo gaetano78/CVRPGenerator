adb8094d7cf8847ef726c5b1719d2a0c55d2ecab
gaetano78 <g.la.delfa@alice.it>
2024-11-15 17:23:04 +0100

    Update CVRPGenUchoa.py

--- CVRPGenUchoa.py
+++ CVRPGenUchoa.py
@@ -23,7 +23,7 @@ st.markdown(
 
 # Aggiungi uno stile personalizzato per un aspetto professionale
 st.markdown(
-    
+    """
     <style>
     .stButton>button {
         background-color: navy;
@@ -47,7 +47,7 @@ st.markdown(
         font-size: 18px;
     }
     </style>
-    ,
+    """,
     unsafe_allow_html=True,
 )
 
